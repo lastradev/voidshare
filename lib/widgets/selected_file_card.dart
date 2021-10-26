ca3c13fd79d76efed3b4a9be7e1d12a8c6c7370c
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-10-26 14:54:19 -0500

    Add card style

--- lib/widgets/selected_file_card.dart
+++ lib/widgets/selected_file_card.dart
@@ -5,19 +5,43 @@ class SelectedFileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20),
       child: Card(
+        elevation: 3,
+        shadowColor: Colors.grey.shade50.withOpacity(0.4),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
         margin: EdgeInsets.zero,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
-              Image.asset(
-                'assets/images/0066.jpg',
-                width: 60,
-                height: 60,
-                fit: BoxFit.cover,
+              Container(
+                margin: const EdgeInsets.all(8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/0066.jpg',
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 5),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'hot_air_balloon.jpg',
+                    style: TextStyle(
+                      color: Colors.grey.shade800,
+                    ),
+                  ),
+                  const Text('219.4 KB'),
+                ],
               )
             ],
           ),
