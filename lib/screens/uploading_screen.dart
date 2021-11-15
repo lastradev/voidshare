91838cab99a32b9ba005af12546d964d63879673
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-11-15 16:14:03 -0600

    Fix bug to avoid screen popping on file upload

--- lib/screens/uploading_screen.dart
+++ lib/screens/uploading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../providers/file_uploader.dart';
+import '../widgets/custom_snack_bars.dart';
 
 /// File uploading screen.
 class UploadingScreen extends StatelessWidget {
@@ -10,76 +12,86 @@ class UploadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final fileUploader = Provider.of<FileUploader>(context);
+    bool uploadAborted = fileUploader.uploadAborted;
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: fileUploader.abortUpload,
+    return WillPopScope(
+      onWillPop: () async {
+        CustomSnackBars.showCustomSnackBar(
+          context,
+          'You must either abort the upload or wait for it to finish.',
+        );
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Uploading'),
+          automaticallyImplyLeading: false,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: fileUploader.abortUpload,
-        backgroundColor: Colors.red.shade400,
-        tooltip: 'Cancel',
-        child: const Icon(Icons.cancel_rounded),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              children: [
-                Text(
-                  'Uploading files',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                const SizedBox(height: 10),
-                const Text('This may take a while...'),
-              ],
-            ),
-            Column(
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    fileUploader.uploadAborted
-                        ? Column(
-                            children: [
-                              Icon(
-                                Icons.cancel_rounded,
-                                color: Colors.red.shade400,
-                                size: 30,
-                              ),
-                              const Text(
-                                'Aborting upload',
-                                style: TextStyle(fontSize: 10),
-                              )
-                            ],
-                          )
-                        : Text(
-                            '${fileUploader.uploadPercentage}%',
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline5!
-                                .copyWith(color: Colors.blue),
-                          ),
-                    SizedBox(
-                      height: 110,
-                      width: 110,
-                      child: CircularProgressIndicator(
-                        value: fileUploader.uploadPercentage / 100,
-                        backgroundColor: Colors.grey,
-                        strokeWidth: 9,
-                        color: Colors.lightBlue,
+        floatingActionButton: FloatingActionButton(
+          onPressed: fileUploader.abortUpload,
+          backgroundColor: Colors.red.shade400,
+          tooltip: 'Cancel',
+          child: const Icon(Icons.cancel_rounded),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Uploading files',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('This may take a while...'),
+                ],
+              ),
+              Column(
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      uploadAborted
+                          ? Column(
+                              children: [
+                                Icon(
+                                  Icons.cancel_rounded,
+                                  color: Colors.red.shade400,
+                                  size: 30,
+                                ),
+                                const Text(
+                                  'Aborting upload',
+                                  style: TextStyle(fontSize: 10),
+                                )
+                              ],
+                            )
+                          : Text(
+                              '${fileUploader.uploadPercentage}%',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline5!
+                                  .copyWith(color: Colors.blue),
+                            ),
+                      SizedBox(
+                        height: 110,
+                        width: 110,
+                        child: CircularProgressIndicator(
+                          value: fileUploader.uploadPercentage / 100,
+                          backgroundColor: Colors.grey,
+                          strokeWidth: 9,
+                          color: uploadAborted
+                              ? Colors.red.shade400
+                              : Colors.lightBlue,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Image.asset('assets/images/waiting.webp', width: 250),
-          ],
+                    ],
+                  ),
+                ],
+              ),
+              Image.asset('assets/images/waiting.webp', width: 250),
+            ],
+          ),
         ),
       ),
     );
