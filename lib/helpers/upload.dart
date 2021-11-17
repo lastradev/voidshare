06603a5b7cadd1839278727c19d9d129773cbdb3
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-11-16 21:20:39 -0600

    Extract upload loop function

---
+++ lib/helpers/upload.dart
@@ -0,0 +1,88 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/file_manager.dart';
+import '../providers/file_uploader.dart';
+import '../screens/error_screen.dart';
+import '../screens/uploaded_screen.dart';
+import '../screens/uploading_screen.dart';
+import '../widgets/custom_snack_bars.dart';
+
+bool isUploading = false;
+
+/// Upload loop triggers when upload button is pressed.
+void beginUpload(BuildContext context) async {
+  final fileManager = Provider.of<FileManager>(context, listen: false);
+  final fileUploader = Provider.of<FileUploader>(context, listen: false);
+
+  if (isUploading) return;
+
+  if (fileManager.files.isExcedingMaxSize) {
+    CustomSnackBars.showCustomSnackBar(
+      context,
+      'Files total size must be under 512 MB.',
+    );
+    return;
+  }
+
+  isUploading = true;
+
+  /// Keep retrying until success or abort.
+  while (true) {
+    try {
+      Navigator.of(context).pushNamed(UploadingScreen.routeName);
+
+      final url = await fileUploader.uploadFiles(fileManager.files);
+
+      Navigator.of(context).pushReplacementNamed(
+        UploadedScreen.routeName,
+        arguments: url.trim(),
+      );
+
+      break;
+
+      /// Catch abort operation.
+    } on HttpException {
+      Navigator.of(context).pop();
+
+      ScaffoldMessenger.of(context).clearSnackBars();
+
+      CustomSnackBars.showCustomSnackBar(context, 'Upload canceled.');
+
+      break;
+
+      /// Connection error.
+    } on SocketException {
+      /// Avoids retry if abort.
+      if (fileUploader.isUploadAborted) {
+        Navigator.of(context).pop();
+        break;
+      }
+
+      final retry = await Navigator.of(context).pushReplacementNamed(
+        ErrorScreen.routeName,
+      );
+
+      if (retry == null) break;
+
+      /// Cache error.
+      ///
+      /// This could happen if user clear files just before uploading.
+    } on FileSystemException {
+      Navigator.of(context).pop();
+
+      ScaffoldMessenger.of(context).clearSnackBars();
+
+      CustomSnackBars.showCustomSnackBar(
+        context,
+        'Could not retrieve file(s).',
+      );
+
+      break;
+    }
+  }
+
+  isUploading = false;
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,14 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../helpers/upload.dart';
 import '../providers/file_manager.dart';
-import '../providers/file_uploader.dart';
-import '../widgets/custom_snack_bars.dart';
 import '../widgets/file_clearer.dart';
 import '../widgets/file_loading_indicator.dart';
 import '../widgets/select_files_container.dart';
 import '../widgets/selected_file_card.dart';
 import '../widgets/terms_subtitle.dart';
-import 'error_screen.dart';
 import 'history_screen.dart';
-import 'uploaded_screen.dart';
-import 'uploading_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -22,71 +16,13 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final fileManager = Provider.of<FileManager>(context);
-    final fileUploader = Provider.of<FileUploader>(context, listen: false);
-    bool isUploading = false;
 
     return Scaffold(
       floatingActionButton: Visibility(
         visible: fileManager.files.isNotEmpty && !fileManager.isLoadingFiles,
         child: FloatingActionButton(
           tooltip: 'Upload files',
-          onPressed: () async {
-            if (isUploading) {
-              return;
-            }
-
-            if (fileManager.files.isExcedingMaxSize) {
-              CustomSnackBars.showCustomSnackBar(
-                context,
-                'Files total size must be under 512 MB.',
-              );
-              return;
-            }
-
-            isUploading = true;
-
-            /// Keep retrying until success or abort.
-            // Note: Should probably move this business logic from this file.
-            while (true) {
-              try {
-                Navigator.of(context).pushNamed(
-                  UploadingScreen.routeName,
-                );
-                final url = await fileUploader.uploadFiles(fileManager.files);
-                Navigator.of(context).pushReplacementNamed(
-                  UploadedScreen.routeName,
-                  arguments: url.trim(),
-                );
-                break;
-                /// Catch abort operation.
-              } on HttpException {
-                Navigator.of(context).pop();
-                CustomSnackBars.showCustomSnackBar(context, 'Upload canceled.');
-                break;
-              } on SocketException {
-                /// Don't retry if abort.
-                if (fileUploader.isUploadAborted) {
-                  Navigator.of(context).pop();
-                  break;
-                }
-
-                final retry = await Navigator.of(context).pushReplacementNamed(
-                  ErrorScreen.routeName,
-                );
-                if (retry == null) break;
-              } on FileSystemException {
-                Navigator.of(context).pop();
-                ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                CustomSnackBars.showCustomSnackBar(
-                  context,
-                  'Could not retrieve file(s).',
-                );
-                break;
-              }
-            }
-
-            isUploading = false;
-          },
+          onPressed: () => beginUpload(context),
           child: const Icon(Icons.file_upload_rounded),
         ),
       ),
